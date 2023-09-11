2884f803038d5c4fc156b4a77fe2d836bd79030f
Haris Sattar <harissattar8@gmail.com>
2023-09-11 00:01:08 -0500

    added all css for second iteration, buttons for dif styles

--- functions.js
+++ functions.js
@@ -1,12 +1,60 @@
 function light_mode() {
     var links = document.getElementsByClassName("page_style");
-    for (var i = 0; i < links.length; ++i) {
-        links[i].setAttribute("href", "./light.css");
+    var currentCSS = links[0].getAttribute("href");
+    if (currentCSS == "./dark.css") {
+        links[0].setAttribute("href", "./light.css");
+        allCSS.push("./light.css");
+        localStorage.setItem("css", "./light.css");
+    }
+    else if (currentCSS == "./style-dark.css") {
+        links[0].setAttribute("href", "./style-light.css");
+        allCSS.push("./style-light.css");
+        localStorage.setItem("css", "./style-light.css");
     }
 }
 function dark_mode() {
     var links = document.getElementsByClassName("page_style");
-    for (var i = 0; i < links.length; ++i) {
-        links[i].setAttribute("href", "./dark.css");
+    var currentCSS = links[0].getAttribute("href");
+    if (currentCSS == "./light.css") {
+        links[0].setAttribute("href", "./dark.css");
+        allCSS.push("./dark.css");
+        localStorage.setItem("css", "./style-dark.css");
     }
+    else if (currentCSS == "./style-light.css") {
+        links[0].setAttribute("href", "./style-dark.css");
+        allCSS.push("./style-dark.css");
+        localStorage.setItem("css", "./style-dark.css");
+    }
+}
+function style1() {
+    var links = document.getElementsByClassName("page_style");
+    var previousCSS = allCSS[allCSS.length - 1];
+    if (previousCSS == "./style-light.css") {
+        links[0].setAttribute("href", "./light.css");
+        allCSS.push("./light.css");
+        localStorage.setItem("css", "./light.css");
+    }
+    else if (previousCSS == "./style-dark.css") {
+        links[0].setAttribute("href", "./dark.css");
+        allCSS.push("./dark.css");
+        localStorage.setItem("css", "./dark.css");
+    }
+}
+function style2() {
+    var links = document.getElementsByClassName("page_style");
+    var previousCSS = allCSS[allCSS.length - 1];
+    if (previousCSS == "./light.css") {
+        links[0].setAttribute("href", "./style-light.css");
+        allCSS.push("./style-light.css");
+        localStorage.setItem("css", "./style-light.css");
+    }
+    else if (previousCSS == "./dark.css") {
+        links[0].setAttribute("href", "./style-dark.css");
+        allCSS.push("./style-dark.css");
+        localStorage.setItem("css", "./style-dark.css");
+    }
+}
+var data = localStorage.getItem("css");
+if (data != null) {
+    var allCSS = [data];
 }
--- functions.ts
+++ functions.ts
@@ -1,16 +1,78 @@
-function light_mode() {
-    let links: HTMLCollectionOf<Element> = document.getElementsByClassName("page_style");
+function light_mode(): void {
+  let links: HTMLCollectionOf<Element> =
+    document.getElementsByClassName("page_style");
 
-    for(let i = 0; i < links.length; ++i){
-      links[i].setAttribute("href", "./light.css");
-  } 
-    
+  let currentCSS: string | null = links[0].getAttribute("href");
+
+  if (currentCSS == "./dark.css") {
+    links[0].setAttribute("href", "./light.css");
+    allCSS.push("./light.css");
+    localStorage.setItem("css", "./light.css");
+  } else if (currentCSS == "./style-dark.css") {
+    links[0].setAttribute("href", "./style-light.css");
+    allCSS.push("./style-light.css");
+    localStorage.setItem("css", "./style-light.css");
+
+  }
 }
 
-function dark_mode() {
-  let links: HTMLCollectionOf<Element> = document.getElementsByClassName("page_style");
+function dark_mode(): void {
+  let links: HTMLCollectionOf<Element> =
+    document.getElementsByClassName("page_style");
 
-    for(let i = 0; i < links.length; ++i){
-      links[i].setAttribute("href", "./dark.css");
-  } 
+  let currentCSS: string | null = links[0].getAttribute("href");
+
+  if (currentCSS == "./light.css") {
+    links[0].setAttribute("href", "./dark.css");
+    allCSS.push("./dark.css");
+    localStorage.setItem("css", "./style-dark.css");
+  } else if (currentCSS == "./style-light.css") {
+    links[0].setAttribute("href", "./style-dark.css");
+    allCSS.push("./style-dark.css");
+    localStorage.setItem("css", "./style-dark.css");
+
+  }
+}
+
+function style1(): void {
+  let links: HTMLCollectionOf<Element> =
+    document.getElementsByClassName("page_style");
+
+  let previousCSS: string | null = allCSS[allCSS.length - 1];
+
+  if (previousCSS == "./style-light.css") {
+    links[0].setAttribute("href", "./light.css");
+    allCSS.push("./light.css");
+    localStorage.setItem("css", "./light.css");
+  } else if (previousCSS == "./style-dark.css") {
+    links[0].setAttribute("href", "./dark.css");
+    allCSS.push("./dark.css");
+    localStorage.setItem("css", "./dark.css");
+
+  }
+}
+
+function style2(): void {
+  let links: HTMLCollectionOf<Element> =
+    document.getElementsByClassName("page_style");
+
+  let previousCSS: string | null = allCSS[allCSS.length - 1];
+
+  if (previousCSS == "./light.css") {
+    links[0].setAttribute("href", "./style-light.css");
+    allCSS.push("./style-light.css");
+    localStorage.setItem("css", "./style-light.css");
+  } else if(previousCSS == "./dark.css"){
+    links[0].setAttribute("href", "./style-dark.css");
+    allCSS.push("./style-dark.css");
+    localStorage.setItem("css", "./style-dark.css");
+
+  }
+}
+
+
+var data: string | null = localStorage.getItem("css");
+
+if(data != null){
+    var allCSS: string[] = [data];
 }
